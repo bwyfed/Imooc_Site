55ddf7aae90cad2dba7c514857f4a059f0cccab5
bwyfed <36172659@qq.com>
2017-12-14 20:46:34 +0800

    使用grunt集成自动重启功能。

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Hello on 2017/12/14.
+ */
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        watch: {
+            jade: {
+                files: ['views/**'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['public/js/**', 'models/**/*.js', 'schemas/**/*.js'],
+                // tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    file: 'app.js',
+                    args: [],
+                    ignoreFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 3000
+                    },
+                    cwd: __dirname
+                }
+            }
+        },
+
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.option('force', true);    //开发时不会因为语法警告而中断grunt服务
+    grunt.registerTask('default',['concurrent']);
+
+};
