372103111a5d36523e0aa3439821e62e93adcf9e
bwyfed <36172659@qq.com>
2017-12-15 11:18:34 +0800

    使用bcrypt进行密码处理。

--- schemas/user.js
+++ schemas/user.js
@@ -2,7 +2,8 @@
  * Created by Hello on 2017/12/14.
  */
 var mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 var UserSchema = new mongoose.Schema({
     name: {
         unique: true,
@@ -23,12 +24,22 @@ var UserSchema = new mongoose.Schema({
 //给模式添加方法
 //每次存储数据之前，都会调用一下这个方法
 UserSchema.pre('save',function(next){
+    var user = this;
     if(this.isNew) {
         //如果数据是新加的话，更新两个时间值
         this.meta.createAt = this.meta.updateAt = Date.now();
     } else {
         this.meta.updateAt = Date.now();
     }
+    bcrypt.genSalt(SALT_WORK_FACTOR,function(err, salt) {
+        if(err) next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if(err) return next(err);
+            user.password = hash;
+            next();
+        })
+    });
     next();
 });
 //增加一些静态方法，经过Model实例化后才有这些方法
