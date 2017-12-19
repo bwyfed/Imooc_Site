efc4f6483abe27984b183fcfbf9e3b748116995d
bwyfed <36172659@qq.com>
2017-12-19 20:51:32 +0800

    实现用户退出功能和视图渲染，删除注释掉的代码。

--- app.js
+++ app.js
@@ -40,6 +40,11 @@ console.log('imooc start on port ' + port);
 app.get('/',function(req, res){
     console.log('user in sesssion:');
     console.log(req.session.user);
+
+    var _user = req.session.user;
+    if(_user) {
+        app.locals.user = _user;    //存储在本地数据中
+    }
     Movie.fetch(function(err,movies){
         if(err) {
             console.log(err);
@@ -49,34 +54,6 @@ app.get('/',function(req, res){
             movies: movies
         })
     });
-    /*res.render('index',{
-        title: 'imooc 首页',
-        movies: [{
-            title: '机械战警',
-            _id: 1,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        },{
-            title: '机械战警',
-            _id: 2,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        },{
-            title: '机械战警',
-            _id: 3,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        },{
-            title: '机械战警',
-            _id: 4,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        },{
-            title: '机械战警',
-            _id: 5,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        },{
-            title: '机械战警',
-            _id: 6,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }]
-    });*/
 });
 
 // signup 注册路由处理
@@ -130,6 +107,12 @@ app.post('/user/signin',function(req, res){
         })
     });
 });
+//logout 登出逻辑
+app.get('/logout', function(req, res){
+   delete req.session.user; //删除会话记录
+   delete app.locals.user;  //删除本地数据
+   res.redirect('/');
+});
 // userlist page
 app.get('/admin/userlist',function(req, res){
     User.fetch(function(err, users) {
@@ -141,24 +124,6 @@ app.get('/admin/userlist',function(req, res){
             users: users
         })
     })
-    /*
-     res.render('list',{
-     title: 'imooc 列表页',
-     movies: [
-     {
-     title: '机械战警',
-     _id: 1,
-     doctor: '何塞·帕迪里亚',
-     country: '美国',
-     year: 2014,
-     poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-     language: '英语',
-     flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-     summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪里亚执导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为了2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
-     }
-     ]
-     });
-     */
 });
 // detail page
 app.get('/movie/:id',function(req, res){
@@ -169,19 +134,6 @@ app.get('/movie/:id',function(req, res){
             movie: movie
         })
     });
-    /*res.render('detail',{
-        title: 'imooc 详情页',
-        movie: {
-            doctor: '何塞·帕迪里亚',
-            country: '美国',
-            title: '机械战警',
-            year: 2014,
-            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-            language: '英语',
-            flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-            summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪里亚执导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为了2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
-        }
-    });*/
 });
 
 // admin page
@@ -266,24 +218,6 @@ app.get('/admin/list',function(req, res){
             movies: movies
         })
     })
-    /*
-    res.render('list',{
-        title: 'imooc 列表页',
-        movies: [
-            {
-                title: '机械战警',
-                _id: 1,
-                doctor: '何塞·帕迪里亚',
-                country: '美国',
-                year: 2014,
-                poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-                language: '英语',
-                flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-                summary: '翻拍自1987年同名科幻经典、由《精英部队》导演何塞·帕迪里亚执导的新版《机械战警》发布首款预告片。大热美剧《谋杀》男星乔尔·金纳曼化身新“机械战警”酷黑战甲亮相，加里·奥德曼、塞缪尔·杰克逊、迈克尔·基顿三大戏骨绿叶护航。预告片末更亮出了本片将登陆IMAX巨幕。新版《机械战警》故事背景跟原版一样，依旧设定在工业城市底特律，但故事年代已由之前设定的2020年变为了2028年，并且故事格局也明显扩大。在片中，金纳曼饰演的好警察墨菲将会被歹徒“杀死”，然后被进行军火开发的机器人公司Omni Corp改造成半人半机器的“机械战警”。'
-            }
-        ]
-    });
-    */
 });
 
 // list delete movie 列表页删除电影
@@ -319,18 +253,5 @@ app.get('/admin/treedata', function(req, res) {
         console.log('平行遍历结果:');
         console.log(newarr);
     });
-    /*
-    Treedata.fetch(function(err,tree){
-        if(err) {
-            console.log(err);
-        }
-        console.log('get tree data:');
-        console.log(tree);
-        var root = 'Books'; //树形根节点
-        res.json({
-            success: 'ok'
-        });
-    });
-    */
 });
 
